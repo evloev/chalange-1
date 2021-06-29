6fe332203a1f963fc20f0dbaf97e08eb7566520e
abdullah <abdullah.evloev.06@mail.ru>
2021-06-29 22:11:16 +0300

    first commit

---
+++ functions.js
@@ -0,0 +1,54 @@
+
+let myFunction = name => 'abdullah'
+console.log(myFunction())
+
+
+let createPost = lastName => {
+return 'evloev'
+}
+console.log(createPost())
+
+
+let addItem = (admin) =>{
+return 'so'+' vu'
+}
+console.log(addItem())
+
+
+let readFile = () => 'nazran'
+
+console.log(readFile())
+
+
+
+ let getElements = (avto,vaz) =>{
+
+return 2114
+}
+console.log(getElements())
+
+
+let createParentElement = () => 'adam'
+
+console.log(createParentElement())
+
+
+
+let myNumber = () => {
+return 756
+}
+console.log(myNumber())
+
+
+let sendMessage = () => 'hello' + ' world'
+console.log(sendMessage())
+
+let nameOutput = () => {
+   return 'assalamu' + ' alaykum'
+}
+ console.log(nameOutput())
+
+ let deduceAge = (age) => {
+    return 28
+ }
+ console.log(deduceAge())
